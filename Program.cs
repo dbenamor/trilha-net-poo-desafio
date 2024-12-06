550c3fe124c74a355eabc63c27e1f6b79ba605ba
Daniel Benamor <dbenamor@users.noreply.github.com>
2024-12-06 12:13:54 -0300

    Ajuste

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
 Console.WriteLine("Nokia:");
-
 Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia Tijolão", imei: "456431461", memoria: 64);
 
 nokia.Ligar();
@@ -13,7 +12,6 @@ Console.WriteLine("\n");
 
 
 Console.WriteLine("Iphone:");
-
 Smartphone iphone = new Iphone(numero: "4987", modelo: "Iphone 15 pro MAX", imei: "481684248", memoria: 128);
 
 iphone.ReceberLigacao();
